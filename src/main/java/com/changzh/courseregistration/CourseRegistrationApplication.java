3251b918d8626b24de85b718e03335c49d417319
changharry <harrychangzh@gmail.com>
2022-10-06 15:56:01 +0800

    password encryption

--- src/main/java/com/changzh/courseregistration/CourseRegistrationApplication.java
+++ src/main/java/com/changzh/courseregistration/CourseRegistrationApplication.java
@@ -2,8 +2,10 @@ package com.changzh.courseregistration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@EnableScheduling
+@SpringBootApplication(exclude = { org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class })
 public class CourseRegistrationApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/changzh/courseregistration/dao/StudentDAOImpl.java
+++ src/main/java/com/changzh/courseregistration/dao/StudentDAOImpl.java
@@ -5,6 +5,7 @@ import com.changzh.courseregistration.entity.Student;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -36,6 +37,9 @@ public class StudentDAOImpl implements StudentDAO{
     @Override
     public void save(Student student) {
         Session session = entityManager.unwrap(Session.class);
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        String encrypted = bCryptPasswordEncoder.encode(student.getPassword());
+        student.setPassword(encrypted);
         session.saveOrUpdate(student);
 
     }
