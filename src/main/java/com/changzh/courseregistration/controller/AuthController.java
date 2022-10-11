92ac52d6d214d72a964c4535d50dd70d1cabd115
changharry <harrychangzh@gmail.com>
2022-10-11 18:10:46 +0800

    Auth controller && custom exception

---
+++ src/main/java/com/changzh/courseregistration/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.changzh.courseregistration.controller;
+
+import com.changzh.courseregistration.exception.UnAuthorizedException;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static com.changzh.courseregistration.util.JwtUtil.verifyToken;
+
+@RestController
+public class AuthController {
+
+
+    @RequestMapping("/auth")
+    public String authStatus(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        if (verifyToken(token) == null) {
+            throw new UnAuthorizedException("Unauthorized!");
+        } else {
+            return "Authorized!";
+        }
+    }
+}
